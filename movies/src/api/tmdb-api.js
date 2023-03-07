68a1481bc3bad14b73a301492dec3a9faebb3a53
Mark <20025214@wit.ie>
2023-03-07 10:40:53 +0000

    Added Upcoming movies page.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -39,4 +39,11 @@ export const getMovies = () => {
         return json.results;
       });
   };
-  
+  export const getUpcoming = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  };
+ // https://api.themoviedb.org/3/movie/upcoming?api_key=…your-key…&language=en-US&page=1
